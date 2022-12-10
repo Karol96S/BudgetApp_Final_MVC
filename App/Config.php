89a004ffb481052af45d9960e2ba49ee6076568d
Karol_S <szypa288@gmail.com>
2022-12-10 02:29:54 +0100

    basic MVC template implemented

---
+++ App/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class Config{
+
+    const DB_HOST = 'localhost';
+
+    const DB_NAME = 'mvclogin';
+
+    const DB_USER = 'mvcuser';
+
+    const DB_PASSWORD = '123';
+
+    const SHOW_ERRORS = true;
+
+    const SECRET_KEY = 'A98A9893AA77C7825324DCAC43946';
+}
---
+++ Core/View.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Core;
+
+class View{
+
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+
+        $file = "../App/Views/$view";  // relative to Core directory
+
+        if (is_readable($file)) {
+            require $file;
+        } else {
+            //echo "$file not found";
+            throw new \Exception("$file not found");
+        }
+    }
+
+    public static function renderTemplate(string $template, array $args = [])
+    {
+        echo static::getTemplate($template, $args);
+    }
+
+    /**
+     * 
+     */
+    public static function getTemplate(string $template, array $args = [])
+    {
+        static $twig = null;
+ 
+        if ($twig === null)
+        {
+            $loader = new \Twig\Loader\FilesystemLoader('../App/Views');
+            $twig = new \Twig\Environment($loader);
+            $twig->addGlobal('current_user', \App\Auth::getUser());
+            $twig->addGlobal('flash_messages', \App\Flash::getMessages());
+        }
+ 
+        return $twig->render($template, $args);
+    }
+}
